0bfc8f1af0fe4a53c38970c9a93248d912e8c96f
pushhok <puskarevamilana85@gmail.com>
2026-05-27 23:11:36 +0300

    Initial commit - Todo Manager App

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/todo-manager/' // 👈 замените на имя вашего GitHub-репозитория
+  base: '/todo-manager/'
 })
